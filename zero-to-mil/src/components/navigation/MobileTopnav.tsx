914b99694c582043215bcb5b29946b70ac8394c1
Shirish754 <shpokh455@gmail.com>
2024-08-04 10:26:38 +0545

    mobileTopNav complete and axios instance initiation

--- zero-to-mil/src/components/navigation/MobileTopnav.tsx
+++ zero-to-mil/src/components/navigation/MobileTopnav.tsx
@@ -1,25 +1,40 @@
 import Image from "next/image";
 import React from "react";
+import DynamicIcon from "../utils/DynamicIcon";
+import { Plus, Trash2 } from "lucide-react";
 
 export interface MobileTopNavProps {
   logoSrc?: string;
+  amount?: number;
 }
 
-function MobileTopNav({ logoSrc = "/ZeroToMilLogo.svg" }: MobileTopNavProps) {
+function MobileTopNav({
+  logoSrc = "/ZeroToMilLogo.svg",
+  amount = 120,
+}: MobileTopNavProps) {
   return (
     <div className="flex justify-between p-3 bg-gray-800">
-      <div className="flex justify-start border ">
-        <div className="flex justify-start h-6 border border-white m-0">
-          <Image src={logoSrc} alt={"logo"} width={100} height={24} priority />
+      <div className="flex justify-start border items-center">
+        <div className="flex justify-start h-6 fit-content m-0 mx-2">
+          <Image src={logoSrc} alt={"logo"} width={24} height={24} priority />
         </div>
-        <div className="text-white">ZeroToMil</div>
+        <div className="text-white font-semibold">Zerotomil</div>
       </div>
 
-      <div className="">
+      <div className="bg-background rounded-full">
         {true ? (
-          <div className="text-white">Menu</div>
+          <div className="flex items-center rounded-full p-2 gap-4">
+            <DynamicIcon
+              IconComponent={Plus}
+              className="text-background bg-white rounded-full"
+            />
+            <p>{amount}$</p>
+          </div>
         ) : (
-          <div className="text-white">Search</div>
+          <div className="text-white">
+            {" "}
+            <DynamicIcon IconComponent={Trash2} className="text-destructive" />
+          </div>
         )}
       </div>
     </div>
---
+++ zero-to-mil/src/lib/axios.ts
@@ -0,0 +1,43 @@
+import { apiRoutes } from "@/constants/apiRoutes";
+import { getApiRoutes } from "@/utils/getRoutes";
+import axios from "axios";
+
+declare module "axios" {
+  interface AxiosRequestConfig {
+    name?: keyof typeof apiRoutes;
+    modifier?: Record<string, string>;
+  }
+}
+
+export const axiosService = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axios.interceptors.request.use(
+  function (config) {
+    if (config.name) {
+      config.url = getApiRoutes(config.name ?? "", config.modifier);
+    }
+    return config;
+  },
+  function (error: Error) {
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+axios.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
