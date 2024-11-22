603b3eb7a26be2c545b5c8ff465445e6f1f52578
Shirish754 <shpokh455@gmail.com>
2024-11-22 23:12:44 +0545

    betslip completion

---
+++ zero-to-mil/src/app/(protected)/betslip/_components/OddList.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+function OddList() {
+  return (
+    <div className="sticky bottom-0 pb-8 mb-11 mx-4">
+      <div className="flex justify-between mb-3 text-lg">
+        <p className="font-semibold">Total Odds</p>
+        <p>25</p>
+      </div>
+      <div className="bg-input w-full rounded mb-4 p-3 text-sm text-cardtitle">
+        <p>Stake amount, $</p>
+        <p>0.00</p>
+      </div>
+      <div className="flex flex-wrap justify-between gap-19 text-base">
+        <p className="bg-input py-1 rounded flex-1 text-center">5</p>
+        <p className="bg-input py-1 rounded flex-1 text-center">10</p>
+        <p className="bg-input py-1 rounded flex-1 text-center">50</p>
+      </div>
+      <div className="flex justify-between pb-4 mt-3 text-lg">
+        <div className="font-semibold">Possible Return</div>
+        <p>$1400</p>
+      </div>
+      <Button variant="secondary" className="w-full mb-4">
+        Place Bet
+      </Button>
+    </div>
+  );
+}
+
+export default OddList;
---
+++ zero-to-mil/src/components/base/card/SlipCards.tsx
@@ -0,0 +1,29 @@
+import DynamicIcon from "@/components/utils/DynamicIcon";
+import { StarIcon } from "lucide-react";
+import React from "react";
+
+function SlipCards() {
+  return (
+    <div className="p-4  bg-menu">
+      <div className="flex justify-between">
+        <div className="flex">
+          <DynamicIcon IconComponent={StarIcon} className={"text-cardtitle"} />
+          <p className="ml-2 text-s text-cardtitle">Real Kashmir vs Trau FC</p>
+        </div>
+        <div className="text-muted">X</div>
+      </div>
+
+      <div className="flex items-center pt-2">
+        <div className=" min-w-12 min-h-11 rounded bg-input text-center flex justify-center  items-center ">
+          <p className="fw-normal">50.5</p>
+        </div>
+        <div className="ml-2 text-xs">
+          <p>Trau FC</p>
+          <p className="text-muted pt-2">1x2</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SlipCards;
