6d6b67e2d27fc1f8fe41367ad1383490de862bae
Shirish754 <shpokh455@gmail.com>
2024-11-27 23:02:27 +0545

    small changes in otp fields

--- zero-to-mil/src/app/(protected)/(forms)/account-setting/page.tsx
+++ zero-to-mil/src/app/(protected)/(forms)/account-setting/page.tsx
@@ -5,7 +5,9 @@ import {
   SetMPINForm,
 } from "@/components/custom/accountSetting";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import DetailWrapper from "@/components/wrapper/detailWrapper";
 import { useLayoutStore } from "@/store/slices/layoutState";
+import { getPageRoutes } from "@/utils/getRoutes";
 import React from "react";
 
 function AccountSetting() {
@@ -13,30 +15,36 @@ function AccountSetting() {
   console.log("width", width, isMobile);
 
   return (
-    <Tabs defaultValue="password" className="relative mr-auto w-full">
-      <TabsList className="w-full flex justify-start rounded-none border-b bg-transparent p-0 ">
-        <TabsTrigger
-          value="password"
-          className="relative flex-grow rounded-none border-b-2 border-b-transparent bg-transparent px-4 pb-3 pt-2 font-semibold text-muted-foreground shadow-none transition-none focus-visible:ring-0 data-[state=active]:border-b-haravara data-[state=active]:text-foreground data-[state=active]:shadow-none "
-        >
-          Change Password
-        </TabsTrigger>
-        <TabsTrigger
-          value="mpin"
-          className="relative flex-grow rounded-none border-b-2 border-b-transparent bg-transparent px-4 pb-3 pt-2 font-semibold text-muted-foreground shadow-none transition-none focus-visible:ring-0 data-[state=active]:border-b-haravara data-[state=active]:text-foreground data-[state=active]:shadow-none "
-        >
-          Change MPIN
-        </TabsTrigger>
-      </TabsList>
-      <TabsContent value="password">
-        <div>
-          <ChangePasswordFrom />
-        </div>
-      </TabsContent>
-      <TabsContent value="mpin">
-        {true ? <ChangeMPINForm /> : <SetMPINForm />}
-      </TabsContent>
-    </Tabs>
+    <DetailWrapper
+      title="Account Settings"
+      navigationLink={getPageRoutes("menu")}
+      enablePadding={false}
+    >
+      <Tabs defaultValue="password" className="relative mr-auto w-full">
+        <TabsList className="w-full flex justify-start rounded-none border-b bg-transparent p-0 ">
+          <TabsTrigger
+            value="password"
+            className="relative flex-grow rounded-none border-b-2 border-b-transparent bg-transparent px-4 pb-3 pt-2 font-semibold text-muted-foreground shadow-none transition-none focus-visible:ring-0 data-[state=active]:border-b-haravara data-[state=active]:text-foreground data-[state=active]:shadow-none "
+          >
+            Change Password
+          </TabsTrigger>
+          <TabsTrigger
+            value="mpin"
+            className="relative flex-grow rounded-none border-b-2 border-b-transparent bg-transparent px-4 pb-3 pt-2 font-semibold text-muted-foreground shadow-none transition-none focus-visible:ring-0 data-[state=active]:border-b-haravara data-[state=active]:text-foreground data-[state=active]:shadow-none "
+          >
+            Change MPIN
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="password">
+          <div>
+            <ChangePasswordFrom />
+          </div>
+        </TabsContent>
+        <TabsContent value="mpin">
+          {true ? <ChangeMPINForm /> : <SetMPINForm />}
+        </TabsContent>
+      </Tabs>
+    </DetailWrapper>
   );
 }
 
