f15f293262a8a7239974d9d0ca630d6e74519be8
Shirish754 <shpokh455@gmail.com>
2024-11-30 22:05:40 +0545

    static access token fetch

--- zero-to-mil/src/app/(protected)/layout.tsx
+++ zero-to-mil/src/app/(protected)/layout.tsx
@@ -10,16 +10,24 @@ import React, { PropsWithChildren, useEffect } from "react";
 function ProtectedLayout({ children }: PropsWithChildren) {
   const { accessToken, setAccessToken } = useAppStore((state) => state);
 
+  // const { mutate } = useAppMutation("access-token", {});
+
   useEffect(() => {
     const fetchAccessToken = async () => {
       try {
         const response = await getAccessToken();
+        console.log("response", response);
         setAccessToken(response);
         setAxiosAuthTokens(response);
       } catch (error) {
         console.error("Failed to fetch access token:", error);
       }
     };
+    // if (!accessToken)
+    //   mutate({
+    //     refresh:
+    //       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTczMzQyMTM5OCwiaWF0IjoxNzMyODE2NTk4LCJqdGkiOiIwMDcxZjMwZjFmNzE0OWYzODgzYTNlMzQxZjNlNDIyZSIsInVzZXJfaWQiOjI4fQ.Iv5gk7ttQ4ZMz9feiwvH8zRZy8H0B9MLEVI03mDeVII",
+    //   });
     if (!accessToken) fetchAccessToken();
   }, [accessToken, setAccessToken]);
 
