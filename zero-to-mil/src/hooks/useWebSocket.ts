68134e12124e06e5002d84407ca10334a12af4b6
Shirish754 <shpokh455@gmail.com>
2024-12-03 08:38:41 +0545

    websocket hook update

--- zero-to-mil/src/hooks/useWebSocket.ts
+++ zero-to-mil/src/hooks/useWebSocket.ts
@@ -1,24 +1,27 @@
 import { useAppStore } from "@/store";
 import { useEffect, useRef, useState } from "react";
 
-interface WebSocketOptions {
+interface WebSocketOptions<T> {
   onOpen?: (event: Event) => void;
   onClose?: (event: CloseEvent) => void;
-  onMessage?: (event: MessageEvent) => void;
+  onMessage?: (event: T) => void;
   onError?: (event: Event) => void;
   headers?: Record<string, string>;
 }
 
-export const useWebSocket = (url: string, options?: WebSocketOptions) => {
+export const useWebSocket = <T = unknown>(
+  url: string,
+  options?: WebSocketOptions<T>
+) => {
   const { accessToken } = useAppStore((state) => state);
   const [isConnected, setIsConnected] = useState(false);
-  const [messages, setMessages] = useState<string[]>([]);
+  const [messages, setMessages] = useState<T[]>([]);
   const socketRef = useRef<WebSocket | null>(null);
   console.log("accessTokenfrom socket", accessToken);
 
   useEffect(() => {
     const socket = new WebSocket(
-      `${process.env.NEXT_PUBLIC_WS_URL}/${url}?token=${accessToken}`
+      `${process.env.NEXT_PUBLIC_WS_URL}${url}?token=${`Bearer ${accessToken}`}`
     );
     socketRef.current = socket;
 
@@ -37,9 +40,14 @@ export const useWebSocket = (url: string, options?: WebSocketOptions) => {
     };
 
     socket.onmessage = (event) => {
-      setMessages((prevMessages) => [...prevMessages, event.data]);
-      if (options?.onMessage) {
-        options.onMessage(event);
+      try {
+        const parsedData = JSON.parse(event.data) as T;
+        setMessages((prevMessages) => [...prevMessages, parsedData]);
+        if (options?.onMessage) {
+          options.onMessage(parsedData);
+        }
+      } catch (error) {
+        console.error("Failed to parse websocket message:", error);
       }
     };
 
@@ -52,13 +60,15 @@ export const useWebSocket = (url: string, options?: WebSocketOptions) => {
     return () => {
       socket.close();
     };
-  }, [url, options]);
+  }, [url, options, accessToken]);
 
   const sendMessage = (message: string) => {
     if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
       socketRef.current.send(message);
     }
   };
+
+  console.log("messages", messages);
 
   return { isConnected, messages, sendMessage };
 };
