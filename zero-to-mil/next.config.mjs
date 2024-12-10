3a826ebc08f6b14be2385fd1033b245fb47752f0
Shirish754 <shpokh455@gmail.com>
2024-12-10 20:20:42 +0545

    next config update for images

--- zero-to-mil/next.config.mjs
+++ zero-to-mil/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        dangerouslyAllowSVG: true,
+        domains: ['placehold.co'],
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+                pathname: '/**',
+            },
+        ],
+    }
+};
 
 export default nextConfig;
