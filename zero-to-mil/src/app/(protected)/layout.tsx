ff8473aa09bd84652c4f1d8aab30797d92549f3d
Shirish754 <shpokh455@gmail.com>
2024-12-10 20:21:31 +0545

    accesstoken refreh token issue solved

--- zero-to-mil/src/app/(protected)/layout.tsx
+++ zero-to-mil/src/app/(protected)/layout.tsx
@@ -5,29 +5,27 @@ import { useAppMutation } from "@/lib/api";
 import { useAppStore } from "@/store";
 import getAccessToken from "@/store/actions/getAccessToken";
 import { setAxiosAuthTokens } from "@/utils/token";
-import React, { PropsWithChildren, useEffect } from "react";
+import React, { PropsWithChildren, useEffect, useRef } from "react";
 
 function ProtectedLayout({ children }: PropsWithChildren) {
   const { accessToken, setAccessToken } = useAppStore((state) => state);
-
+  const fetchingRef = useRef(false);
   // const { mutate } = useAppMutation("access-token", {});
 
   useEffect(() => {
     const fetchAccessToken = async () => {
+      if (fetchingRef.current) return;
       try {
+        fetchingRef.current = true;
         const response = await getAccessToken();
-        console.log("response", response);
         setAccessToken(response);
         setAxiosAuthTokens(response);
       } catch (error) {
         console.error("Failed to fetch access token:", error);
+      } finally {
+        fetchingRef.current = false;
       }
     };
-    // if (!accessToken)
-    //   mutate({
-    //     refresh:
-    //       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTczMzQyMTM5OCwiaWF0IjoxNzMyODE2NTk4LCJqdGkiOiIwMDcxZjMwZjFmNzE0OWYzODgzYTNlMzQxZjNlNDIyZSIsInVzZXJfaWQiOjI4fQ.Iv5gk7ttQ4ZMz9feiwvH8zRZy8H0B9MLEVI03mDeVII",
-    //   });
     if (!accessToken) fetchAccessToken();
   }, [accessToken, setAccessToken]);
 
