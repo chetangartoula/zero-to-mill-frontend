c8c891ba4a6203e6be0022974fc2e4938b4e9eb9
Shirish754 <shpokh455@gmail.com>
2025-03-07 21:05:40 +0545

    build issue fix

---
+++ zero-to-mil/src/app/(guest)/forgot-password/new-password/_component/form.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import InputFormField from "@/components/formfields/InputFormField";
+import { Button } from "@/components/ui/button";
+import { newPasswordSchema } from "@/schemas/auth";
+import AppForm from "@/components/base/form/AppForm";
+import FormContentWrapper from "@/components/wrapper/formContentWrapper";
+import { useAppMutation } from "@/lib/api";
+import { useRouter, useSearchParams } from "next/navigation";
+import { getPageRoutes } from "@/utils/getRoutes";
+import { isString } from "lodash";
+import { useToast } from "@/components/ui/use-toast";
+
+function NewPasswordForm() {
+  const router = useRouter();
+  const { mutate } = useAppMutation("forgotPassword");
+  const { toast } = useToast();
+  const searchParams = useSearchParams();
+  const ssid = searchParams.get("ssid");
+  return (
+    <AppForm
+      defaultValues={{
+        password: "",
+        passwordConfirmation: "",
+      }}
+      schema={newPasswordSchema}
+      onSubmit={(data, form) =>
+        mutate(
+          {
+            ssid: "",
+            new_password: data.password,
+            confirm_password: data.passwordConfirmation,
+          },
+          {
+            onSuccess: () => {
+              toast({
+                title: "Password Set",
+                description: "You can now login with your new password",
+                variant: "success",
+              });
+              router.push(getPageRoutes("login"));
+            },
+            onError: (error: any) => {
+              if (error.response?.data) {
+                Object.entries(error.response.data).forEach(([key, value]) => {
+                  form.setError(key as any, {
+                    type: "manual",
+                    message: value as string,
+                  });
+                });
+              } else {
+                isString(error) &&
+                  toast({
+                    title: "Error",
+                    description: error,
+                    variant: "destructive",
+                  });
+              }
+            },
+          }
+        )
+      }
+    >
+      <FormContentWrapper>
+        {" "}
+        <InputFormField name="password" label="Password" type="password" />
+        <InputFormField
+          name="passwordConfirmation"
+          label="Confirm Password"
+          type="password"
+        />
+      </FormContentWrapper>
+
+      <Button size={"full"}>Set New Password</Button>
+    </AppForm>
+  );
+}
+
+export default NewPasswordForm;
--- zero-to-mil/src/app/(guest)/forgot-password/new-password/page.tsx
+++ zero-to-mil/src/app/(guest)/forgot-password/new-password/page.tsx
@@ -1,88 +1,23 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import AuthWrapper from "@/components/wrapper/authWrapper";
-import InputFormField from "@/components/formfields/InputFormField";
-import { Button } from "@/components/ui/button";
-import { newPasswordSchema } from "@/schemas/auth";
-import AppForm from "@/components/base/form/AppForm";
-import FormContentWrapper from "@/components/wrapper/formContentWrapper";
-import { useAppMutation } from "@/lib/api";
-import { useRouter, useSearchParams } from "next/navigation";
-import { getPageRoutes } from "@/utils/getRoutes";
-import { isString } from "lodash";
-import { useToast } from "@/components/ui/use-toast";
+
+import Loading from "@/components/loading";
+import NewPasswordForm from "./_component/form";
 
 function NewPasswordSetUp() {
-  const router = useRouter();
-  const { mutate } = useAppMutation("forgotPassword");
-  const { toast } = useToast();
-  const ssid = useSearchParams().get("ssid") || "";
   return (
-    <AuthWrapper
-      title="Set New Password"
-      wrapperProps={{
-        className:
-          "relative sm:top-20 md:top-20 lg:top-0 xl:top-0 2xl:top-0 m-4",
-      }}
-    >
-      <AppForm
-        defaultValues={{
-          password: "",
-          passwordConfirmation: "",
+    <Suspense fallback={<Loading />}>
+      <AuthWrapper
+        title="Set New Password"
+        wrapperProps={{
+          className:
+            "relative sm:top-20 md:top-20 lg:top-0 xl:top-0 2xl:top-0 m-4",
         }}
-        schema={newPasswordSchema}
-        onSubmit={(data, form) =>
-          mutate(
-            {
-              ssid: ssid,
-              new_password: data.password,
-              confirm_password: data.passwordConfirmation,
-            },
-            {
-              onSuccess: () => {
-                toast({
-                  title: "Password Set",
-                  description: "You can now login with your new password",
-                  variant: "success",
-                });
-                router.push(getPageRoutes("login"));
-              },
-              onError: (error: any) => {
-                if (error.response?.data) {
-                  Object.entries(error.response.data).forEach(
-                    ([key, value]) => {
-                      form.setError(key as any, {
-                        type: "manual",
-                        message: value as string,
-                      });
-                    }
-                  );
-                } else {
-                  isString(error) &&
-                    toast({
-                      title: "Error",
-                      description: error,
-                      variant: "destructive",
-                    });
-                }
-              },
-            }
-          )
-        }
       >
-        <FormContentWrapper>
-          {" "}
-          <InputFormField name="password" label="Password" type="password" />
-          <InputFormField
-            name="passwordConfirmation"
-            label="Confirm Password"
-            type="password"
-          />
-        </FormContentWrapper>
-
-        <Button size={"full"}>Set New Password</Button>
-      </AppForm>
-    </AuthWrapper>
+        <NewPasswordForm />
+      </AuthWrapper>
+    </Suspense>
   );
 }
 
