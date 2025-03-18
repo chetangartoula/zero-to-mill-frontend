735f3856f08d7243f928761148850a5dac7ae5a6
Shirish754 <shpokh455@gmail.com>
2025-03-18 21:21:17 +0545

    next config update

--- zero-to-mil/next.config.mjs
+++ zero-to-mil/next.config.mjs
@@ -14,6 +14,10 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'ba5.zerotomil.com',
                 pathname: '/**',
+            }, {
+                protocol: 'https',
+                hostname: 'www.zerotomil.com',
+                pathname: '/**',
             }
         ],
     }
