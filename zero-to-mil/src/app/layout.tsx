d78c2e10e5c2ef743026b0d702ebcadba626a980
Shirish <85387438+Shirish754@users.noreply.github.com>
2025-08-20 22:41:17 +0545

    Pwa (#3)
    * initiate pwa
    
    * images
    
    * seo update

--- zero-to-mil/src/app/layout.tsx
+++ zero-to-mil/src/app/layout.tsx
@@ -3,8 +3,62 @@ import "./globals.css";
 import { AppProvider } from "./provider";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import { Metadata, Viewport } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    default: "Zero To Mil",
+    template: "%s | Zero To Mil",
+  },
+  description: "A Progressive Web App built with Next.js App Router and pnpm",
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "Zero To Mil",
+    startupImage: [
+      {
+        url: "/splash-640x1136.png",
+        media:
+          "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)",
+      },
+    ],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: "Zero To Mil",
+    title: {
+      default: "Zero To Mil",
+      template: "%s | Zero To Mil",
+    },
+    description: "Zero To MIl.",
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: "Zero To Mil",
+      template: "%s | Zero To Mil",
+    },
+    description: "Zero To Mil",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  minimumScale: 1,
+  initialScale: 1,
+  width: "device-width",
+  // shrinkToFit: false,
+  viewportFit: "cover",
+};
 
 export default function RootLayout({
   children,
